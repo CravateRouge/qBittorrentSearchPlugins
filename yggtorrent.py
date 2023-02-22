87cf323c824ab94983c60e709ed2c0ede7690a06
CravateRouge <cravaterouge@users.noreply.github.com>
2023-02-22 19:26:39 +0000

    Automated url update from https://www5.yggtorrent.fi to https://www6.yggtorrent.lol/

--- yggtorrent.py
+++ yggtorrent.py
@@ -45,7 +45,7 @@ class yggtorrent(object):
     password = "YOUR PASSWORD"
    ###########################################################################
 
-    url = 'https://www5.yggtorrent.fi'
+    url = 'https://www6.yggtorrent.lol/'
     name = 'YGG Torrent'
     supported_categories = {'all': '', 'music': '2139', 'movies': '2145', 'games':'2142', 'software': '2144', 'books': '2140'}
 
