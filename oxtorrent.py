24eb1bb77c39dead67d6a3583afa6d0144cdf2fa
CravateRouge <cravaterouge@users.noreply.github.com>
2023-03-08 11:47:04 +0000

    Automated url update from https://www.oxtorrent.sk to https://oxtorrent.tk

--- oxtorrent.py
+++ oxtorrent.py
@@ -34,7 +34,7 @@ from novaprinter import prettyPrinter
 
 class oxtorrent(object):
     """ Search engine class """
-    url = 'https://www.oxtorrent.sk'
+    url = 'https://oxtorrent.tk'
     name = 'OxTorrent'
     supported_categories = {'all': '', 'music': 'musique', 'movies': 'films',
                             'books': 'ebook', 'software': 'logiciels', 'tv': 'series'}
