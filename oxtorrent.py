bc740978796ff2e4a0bb60d387dfa866f1ee8bd1
CravateRouge <cravaterouge@users.noreply.github.com>
2024-06-19 11:47:13 +0000

    Automated url update from https://www.torrent.ws/Torrent911 to https://www.protege-torrent.com/Torrent911

--- oxtorrent.py
+++ oxtorrent.py
@@ -1,4 +1,4 @@
-#VERSION: 2.16
+#VERSION: 2.17
 # AUTHORS: Gandalf (github.com/erdoukki)
 # CONTRIBUTORS: CravateRouge (github.com/CravateRouge)
 
@@ -34,7 +34,7 @@ from novaprinter import prettyPrinter
 
 class oxtorrent(object):
     """ Search engine class """
-    url = 'https://www.torrent.ws/Torrent911'
+    url = 'https://www.protege-torrent.com/Torrent911'
     name = 'OxTorrent'
     supported_categories = {'all': '', 'music': 'musique', 'movies': 'films',
                             'books': 'ebook', 'software': 'logiciels', 'tv': 'series'}
