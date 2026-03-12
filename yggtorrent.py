6c686d91c04ef5a51f9ab12fcdf3d887589f5344
CravateRouge <cravaterouge@users.noreply.github.com>
2026-03-12 11:56:29 +0000

    Automated url update from https://www.yggtorrent.org/auth/login to https://ygg.guru

--- yggtorrent.py
+++ yggtorrent.py
@@ -1,4 +1,4 @@
-#VERSION: 1.5
+#VERSION: 1.6
 #AUTHORS: CravateRouge (github.com/CravateRouge)
 
 # Redistribution and use in source and binary forms, with or without
@@ -45,7 +45,7 @@ class yggtorrent(object):
     password = "YOUR PASSWORD"
    ###########################################################################
 
-    url = 'https://www.yggtorrent.org/auth/login'
+    url = 'https://ygg.guru'
     name = 'YGG Torrent'
     supported_categories = {'all': '', 'music': '2139', 'movies': '2145', 'games':'2142', 'software': '2144', 'books': '2140'}
 
